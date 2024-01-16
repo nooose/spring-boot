3606944878337293c7cbac91e8e666be6b60441a
nooose <hd15807@gmail.com>
2024-01-17 00:47:37 +0900

    feat: 외부 설정 예제
    Environment 없이 직접 K:V 조회

---
+++ src/test/java/com/example/demo/external/JavaSystemProperties.java
@@ -0,0 +1,27 @@
+package com.example.demo.external;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Map;
+import java.util.Properties;
+
+@Slf4j
+public class JavaSystemProperties {
+    public static void main(String[] args) {
+        Properties properties = System.getProperties();
+        for (Object key : properties.keySet()) {
+            log.info("prop {}={}", key, System.getProperty(String.valueOf(key)));
+        }
+
+        String url = System.getProperty("url");
+        String username = System.getProperty("username");
+        String password = System.getProperty("password");
+        log.info("url = {}", url);
+        log.info("username = {}", username);
+        log.info("password = {}", password);
+
+        System.setProperty("hello", "Hello, value");
+        String hello = System.getProperty("hello");
+        log.info("hello = {}", hello);
+    }
+}
