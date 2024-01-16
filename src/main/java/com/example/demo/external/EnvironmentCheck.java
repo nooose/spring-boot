fd419a5be46e932432594510be5747d05c1798e1
nooose <hd15807@gmail.com>
2024-01-17 00:56:26 +0900

    feat: Environment 예제

---
+++ src/main/java/com/example/demo/external/EnvironmentCheck.java
@@ -0,0 +1,29 @@
+package com.example.demo.external;
+
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class EnvironmentCheck {
+
+    private final Environment environment;
+
+    public EnvironmentCheck(Environment environment) {
+        this.environment = environment;
+    }
+
+    @PostConstruct
+    public void init() {
+        String url = environment.getProperty("url");
+        String username = environment.getProperty("username");
+        String password = environment.getProperty("password");
+        String memory = environment.getProperty("memory");
+        log.info("env url = {}", url);
+        log.info("env username = {}", username);
+        log.info("env password = {}", password);
+        log.info("env memory = {}", memory);
+    }
+}
